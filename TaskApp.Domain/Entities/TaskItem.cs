4ebb68647b5b133f9067141e9dcc65ec300dbe35
MarekAbel2702 <marek-abel@wp.pl>
2025-06-08 00:20:02 +0200

    Dodanie encji, folderu common i klasy z używanymi właściwościami oraz interfejsu

---
+++ TaskApp.Domain/Entities/TaskItem.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskApp.Domain.Common;
+
+namespace TaskApp.Domain.Entities
+{
+    public class TaskItem : BaseEntity
+    {
+        public string Title { get; set; } = string.Empty;
+        public string? Description {  get; set; }
+        public bool IsDone { get; set; } = false;
+        public DateTime? DueDate { get; set; }
+
+        public void MarkAsDone()
+        {
+            IsDone = true;
+        }
+        public void MarkAsUndone()
+        {
+            IsDone = false;
+        }
+    }
+}
---
+++ TaskApp.Domain/Interfaces/ITaskRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskApp.Domain.Entities;
+
+namespace TaskApp.Domain.Interfaces
+{
+    public interface ITaskRepository
+    {
+        Task<TaskItem?> GetByIdAsync(Guid id);
+        Task<List<TaskItem>> GetAllAsync();
+        Task AddAsync (TaskItem task);
+        Task UpdateAsync (TaskItem task);
+        Task DeleteAsync (Guid id);
+    }
+}
