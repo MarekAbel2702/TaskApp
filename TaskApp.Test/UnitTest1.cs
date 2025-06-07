670b380bf7355b65097e2eeb357217c1f4562288
MarekAbel2702 <marek-abel@wp.pl>
2025-06-07 23:49:31 +0200

    Dodaj pliki projektów.

---
+++ TaskApp.Test/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace TaskApp.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ TaskApp.Tests/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace TaskApp.Tests
+{
+    public class Class1
+    {
+
+    }
+}
