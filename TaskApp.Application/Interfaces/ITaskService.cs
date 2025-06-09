44db2a23069e423991dd8204a800b0d2be446fba
MarekAbel2702 <marek-abel@wp.pl>
2025-06-09 23:51:16 +0200

    Dodanie interfejsu w warstwie application

---
+++ TaskApp.Application/Interfaces/ITaskService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskApp.Application.DTOs;
+
+namespace TaskApp.Application.Interfaces
+{
+    public interface ITaskService
+    {
+        Task<List<TaskItemDto>> GetAllTasksAsync();
+        Task<TaskItemDto?> GetTaskByIdAsync(Guid id);
+        Task<TaskItemDto> CreateTaskAsync(Task item);
+        Task<bool> DeleteTaskAsync(Guid id);
+        Task<bool> MarkAsDoneAsync(Guid id);
+        Task<bool> MarkAsUndoneAsync(Guid id);
+    }
+}
