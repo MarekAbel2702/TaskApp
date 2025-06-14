b868addbdc61531ca9f3648ab22a16ab7e9a7d43
MarekAbel2702 <marek-abel@wp.pl>
2025-06-14 18:22:44 +0200

    Dodanie mapowania

---
+++ TaskApp.Application/Mappings/TaskItemProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskApp.Application.DTOs;
+using TaskApp.Domain.Entities;
+
+namespace TaskApp.Application.Mappings
+{
+    public class TaskItemProfile : Profile
+    {
+        public TaskItemProfile() 
+        {
+            CreateMap<TaskItem, TaskItemDto>();
+            CreateMap<CreateTaskItemDto, TaskItem>();
+        }
+    }
+}
